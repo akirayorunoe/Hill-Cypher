ed5801ca479382cd432b786047fd6b911c96a653
= <=>
2019-10-03 13:45:05 +0700

    Fix : hill Cypher

--- hillCypher/hillCypher.cpp
+++ hillCypher/hillCypher.cpp
@@ -34,13 +34,50 @@ string formatString(string a, int &l)
 	return a;
 }
 
+int** createMatrix(string a,int s)
+{
+	int **m,index=0,temp=0;
+	m = new int*[s];
+	int columns = a.length() / s;
+	for (int i = 0; i<s; i++)
+	{
+		m[i] = new int[columns];		
+		for (int j = 0; j<columns; j++)		
+		{
+			m[i][j] = a[index];
+			index += s;
+		}
+		index = ++temp;
+	}
+	return m;
+}
+
+void printMatrix(int **a, int hang, int cot) {
+	for (int i = 0; i<hang; i++)
+	{
+		for (int j = 0; j<cot; j++)
+			cout << a[i][j] << "\t";	
+		cout << "\n";						
+	}
+}
+
 int main()
 {
+	int size;
+	//input
 	string str;
-	cout << "Nhap chuoi:";
+	cout << "Nhap chuoi: ";
 	getline(cin,str);
+	cout <<endl<< "Nhap size la 2 hoac 3";
+	do {
+		cout << endl << "Nhap size: ";
+		cin >> size;
+	} while (size != 2 && size != 3);
+	//serve problem 
 	int l = str.length();
 	str = formatString(str, l);
-	cout << str<<endl;
+	cout << str<<endl<<"Size: "<<size<<endl;
+	int **m = createMatrix(str, size);
+	printMatrix(m, size, str.length()/size);
 	system("pause");
 }
